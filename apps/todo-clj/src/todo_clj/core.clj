604b61950a99907792f443b7217feb35a6ffe90d
Takeuchi-Akihide <htasn1110@gmail.com>
2023-05-05 17:54:59 +0900

    ルーティングを一通り定義

--- apps/todo-clj/src/todo_clj/core.clj
+++ apps/todo-clj/src/todo_clj/core.clj
@@ -1,6 +1,29 @@
-(ns todo-clj.core)
+;; "Hello, world"が書かれたサーバの定義
+;; 起動/終了/再起動が行える
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns todo-clj.core 
+  (:require [compojure.core :refer [routes]]
+            [ring.adapter.jetty :as server]
+            [todo-clj.handler.main :refer [main-routes]]
+            [todo-clj.handler.todo :refer [todo-routes]]))
+
+(defonce server (atom nil))
+
+(def app
+  (routes
+   todo-routes
+   main-routes))
+
+(defn start-server []
+  (when-not @server
+    (reset! server (server/run-jetty #'app {:port 3000 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
